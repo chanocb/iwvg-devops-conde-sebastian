5d38821a686caddc6d56472a0de281e907b52a73
chanocb <101331282+chanocb@users.noreply.github.com>
2024-09-18 13:41:39 +0200

    Added FractionTest #4

---
+++ src/test/java/DevOps/FractionTest.java
@@ -0,0 +1,51 @@
+package DevOps;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FractionTest {
+
+    private Fraction fraction;
+
+    @BeforeEach
+    void before() {
+        fraction = new Fraction(3, 4);
+    }
+
+    @Test
+    void testFractionIntInt() {
+        assertEquals(3, fraction.getNumerator());
+        assertEquals(4, fraction.getDenominator());
+    }
+
+    @Test
+    void testFractionDefaultConstructor() {
+        fraction = new Fraction();
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
+    }
+
+    @Test
+    void testDecimal() {
+        assertEquals(0.75, fraction.decimal(), 1e-5);
+    }
+
+    @Test
+    void testSetNumerator() {
+        fraction.setNumerator(5);
+        assertEquals(5, fraction.getNumerator());
+    }
+
+    @Test
+    void testSetDenominator() {
+        fraction.setDenominator(6);
+        assertEquals(6, fraction.getDenominator());
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("Fraction{numerator=3, denominator=4}", fraction.toString());
+    }
+}
